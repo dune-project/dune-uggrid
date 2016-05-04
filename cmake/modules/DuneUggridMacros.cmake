5a3c785a2f8548be1dc45a8f2d6bc50034485949
Markus Blatt <markus@dr-blatt.de>
2016-05-04 11:01:45 +0200

    Make dune-uggrid detection compatible with the old dune-grid detection

--- cmake/modules/DuneUggridMacros.cmake
+++ cmake/modules/DuneUggridMacros.cmake
@@ -1,13 +1,17 @@
 find_package(X11)
 
-if(MPI_FOUND)
-  add_definitions("-DModelP")
-endif()
-
 set(UG_ENABLE_SYSTEM_HEAP True CACHE BOOL
   "Whether to use the UG heap or the one of the operating system")
 
+# Compatibility against previous UG versions.
 set(UG_USE_SYSTEM_HEAP ${UG_ENABLE_SYSTEM_HEAP})
+set(UG_FOUND True)
+set(HAVE_UG True)
+set(UG_VERSION "${DUNE_UGGRID_VERSION}")
+add_definitions("-DENABLE_UG=1")
+if(MPI_FOUND)
+  add_definitions("-DModelP")
+endif()
 
 message("UG_USE_SYSTEM_HEAP=${UG_USE_SYSTEM_HEAP}")
 message("UG_ENABLE_SYSTEM_HEAP=${UG_ENABLE_SYSTEM_HEAP}|")
