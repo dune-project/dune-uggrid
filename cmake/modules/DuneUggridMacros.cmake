8c78d3ea3b28489fe47c6616a8c20976a37037c7
Markus Blatt <markus@dr-blatt.de>
2016-05-04 16:38:44 +0200

    Cleanup and spelling fixes.

--- cmake/modules/DuneUggridMacros.cmake
+++ cmake/modules/DuneUggridMacros.cmake
@@ -11,7 +11,7 @@ set(UG_VERSION "${DUNE_UGGRID_VERSION}")
 
 if(UG_PARALLEL STREQUAL "yes")
   # Actually we probably should activate UG
-  # for everything. But for the being we fall
+  # for everything. But for the time being we fall
   # back to the enable trick. To change this just
   # uncomment the lines below.
   #add_definitions("-DENABLE_UG=1")
@@ -19,12 +19,9 @@ if(UG_PARALLEL STREQUAL "yes")
   set(UG_DEFINITIONS "ENABLE_UG=1;ModelP")
 else()
   # Actually we probably should activate UG
-  # for everything. But for the being we fall
+  # for everything. But for the time being we fall
   # back to the enable trick. To change this just
   # uncomment the lines below.
   #add_definitions("-DENABLE_UG=1")
   set(UG_DEFINITIONS "ENABLE_UG=1")
 endif()
-
-message("UG_USE_SYSTEM_HEAP=${UG_USE_SYSTEM_HEAP}")
-message("UG_ENABLE_SYSTEM_HEAP=${UG_ENABLE_SYSTEM_HEAP}|")
