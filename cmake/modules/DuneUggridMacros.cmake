47bb178cfe378ae2f46a388367f562390868c4f1
Christoph Grüninger <gruenich@dune-project.org>
2016-08-08 15:38:00 +0200

    [CMake] Don't add include paths that are no longer provided
    This fixes missing-include-dir warnings in (some) dune-uggrid
    and (all) dune-grid

--- cmake/modules/DuneUggridMacros.cmake
+++ cmake/modules/DuneUggridMacros.cmake
@@ -31,10 +31,10 @@ if(NOT dune-uggrid_INSTALLED)
   include_directories(${dune-uggrid_PREFIX} ${dune-uggrid_PREFIX}/low
     ${dune-uggrid_PREFIX}/gm ${dune-uggrid_PREFIX}/dev ${dune-uggrid_PREFIX}/dom
     ${dune-uggrid_PREFIX}/np ${dune-uggrid_PREFIX}/ui
-    ${dune-uggrid_PREFIX}/graphics/uggraph ${dune-uggrid_PREFIX}/np/algebra
-    ${dune-uggrid_PREFIX}/np/udm ${dune-uggrid_PREFIX}/np/procs
+    ${dune-uggrid_PREFIX}/np/algebra
+    ${dune-uggrid_PREFIX}/np/udm
     ${dune-uggrid_PREFIX}/parallel ${dune-uggrid_PREFIX}/parallel/ddd
-    ${dune-uggrid_PREFIX}/parallel/ppif ${dune-uggrid_PREFIX}/ddd/include
+    ${dune-uggrid_PREFIX}/parallel/ppif
     ${dune-uggrid_PREFIX}/parallel/dddif ${dune-uggrid_PREFIX}/parallel/util
     ${dune-uggrid_PREFIX}/parallel/ddd/include )
 endif(NOT dune-uggrid_INSTALLED)
