58f380a8951c6fe1cfb6235b21090a15e0c7aa90
Ansgar Burchardt <Ansgar.Burchardt@tu-dresden.de>
2017-03-16 20:25:28 +0100

    Remove `l_amgmatrix_collect`

--- np/np.h
+++ np/np.h
@@ -146,7 +146,6 @@ INT a_vector_collect_noskip (MULTIGRID *mg, INT fl, INT tl, const VECDATA_DESC *
 INT l_ghostvector_collect (GRID *g, const VECDATA_DESC *x);
 INT l_ghostmatrix_collect (GRID *g, const MATDATA_DESC *A);
 INT a_vector_vecskip (MULTIGRID *mg, INT fl, INT tl, const VECDATA_DESC *x);
-INT l_amgmatrix_collect (GRID *g, const MATDATA_DESC *A);
 int DDD_InfoPrioCopies (DDD_HDR hdr);
 INT a_elementdata_consistent (MULTIGRID *mg, INT fl, INT tl);
 INT a_nodedata_consistent (MULTIGRID *mg, INT fl, INT tl);
