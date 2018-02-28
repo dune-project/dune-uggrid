b8659a16ec21b193cd2b0708727db7dfaa60dd7f
Ansgar Burchardt <Ansgar.Burchardt@tu-dresden.de>
2018-02-28 13:27:06 +0100

    add context to global reduction functions
    Also reimplement them using `MPI_Allreduce`.

--- parallel/ddd/ctrl/cons.cc
+++ parallel/ddd/ctrl/cons.cc
@@ -693,7 +693,7 @@ static int ConsCheckDoubleObj (void)
    @returns  total number of errors (sum of all procs)
  */
 
-int DDD_ConsCheck(const DDD::DDDContext&)
+int DDD_ConsCheck(const DDD::DDDContext& context)
 {
   int cpl_errors;
   int total_errors=0;
@@ -733,7 +733,7 @@ int DDD_ConsCheck(const DDD::DDDContext&)
 
 
   /* compute sum of errors over all processors */
-  total_errors = ddd_GlobalSumInt(total_errors);
+  total_errors = ddd_GlobalSumInt(context, total_errors);
 
   DDD_Flush();
   Synchronize();
