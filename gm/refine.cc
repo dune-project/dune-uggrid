dfb456dfcf08e75d6e8fde32ea11798a76569c0d
Henrik Stolzmann <henrik.stolzmann@mailbox.tu-dresden.de>
2019-04-16 20:29:58 +0200

    Fixed warning: "unused-function" caused by "Write_RefRules"
    Added a preprocessor if condition around the definition of
    "Write_RefRules" because it is used in such an environment.
    ````
    static INT Write_RefRules (MULTIGRID *theMG, INT *RefRuleOffset, INT MarkKey)
    {
      ...
    }
    ````
    
    Furthermore there is a comment adressing the only use of "Write_RefRules".
    
    ````
    /* TODO (HRR 971209): drop old call of Write_RefRules */
      #if EXTRACT_RULES
    if (NEW_Write_RefRules(theMG,rr_rule_offsets,MarkKey,&rr_rules)) REP_ERR_RETURN(1)       ;
          #else
    if (Write_RefRules(theMG,rr_rule_offsets,MarkKey)) REP_ERR_RETURN(1);
          #endif
    ````
    But i am not sure whether "Write_RefRules" is still needed.

--- gm/refine.cc
+++ gm/refine.cc
@@ -6096,7 +6096,6 @@ static void CheckConsistency (MULTIGRID *theMG, INT level ,INT debugstart, INT g
 }
 #endif
 
-
 static INT CheckMultiGrid (MULTIGRID *theMG)
 {
   INT level;
@@ -6114,7 +6113,6 @@ static INT CheckMultiGrid (MULTIGRID *theMG)
 
   return(0);
 }
-
 
 #ifdef STAT_OUT
 void NS_DIM_PREFIX Manage_Adapt_Timer (int alloc)
