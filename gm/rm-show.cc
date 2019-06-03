e352a85156e19198c68e5ae08956bbcaf25f859a
Henrik Stolzmann <henrik.stolzmann@mailbox.tu-dresden.de>
2019-06-03 22:56:38 +0200

    build tetrahedron refinement rules into library
    The `TET_RULESET` compile-time option could be used to load
    tetrahedron refinement rules from `RefRules.data`, however this
    requires to locate the file at run-time.  This was too inconvenient.
    
    Instead generate `RefRules.cc` from `RefRules.data` including the
    refinement rules and use that.

---
+++ gm/rm-show.cc
@@ -0,0 +1,46 @@
+#include "config.h"
+
+#include <cerrno>
+#include <cstring>
+
+#include <dune/common/exceptions.hh>
+
+#include "low/namespace.h"
+#include "low/initlow.h"
+#include "dev/ugdevices.h"
+#include "gm/initgm.h"
+#include "gm/rm.h"
+
+#include "gm/rm-write2file.h"
+
+int main(int argc, char** argv)
+{
+  UG::InitLow();
+  UG::InitDevices();
+  NS_DIM_PREFIX InitGm();
+
+  std::vector<NS_DIM_PREFIX REFRULE> rules;
+  std::vector<NS_PREFIX SHORT> patterns;
+
+  // use an arbitrary additional argument to use the rules from "lib/ugdata/RefRules.data"
+  if( argc > 1 )
+  {
+    // read the file "lib/ugdata/RefRules.data" and use those rules
+    std::FILE* stream = std::fopen(argv[1], "r");
+    if (!stream)
+      DUNE_THROW(Dune::Exception, "Could not open file " << argv[1] << ": " << std::strerror(errno));
+    NS_DIM_PREFIX readTetrahedronRules(stream, rules, patterns);
+    std::fclose(stream);
+
+    NS_DIM_PREFIX RefRules[NS_DIM_PREFIX TETRAHEDRON] = rules.data();
+    NS_DIM_PREFIX MaxRules[NS_DIM_PREFIX TETRAHEDRON] = rules.size();
+  }
+
+  // write rules
+  for(int i=0; i<NS_DIM_PREFIX MaxRules[NS_DIM_PREFIX TETRAHEDRON]; i++)
+  {
+    NS_DIM_PREFIX ShowRefRuleX(NS_DIM_PREFIX TETRAHEDRON, i, std::printf);
+  }
+
+  return 0;
+}
