8a9a481a2263dc28addadae376a36f2ef2dac277
Oliver Sander <oliver.sander@tu-dresden.de>
2020-04-04 11:30:32 +0200

    Do not store the format object in the environment data structure
    Instead, we let the multigrid store them directly.  That is
    much simpler.

--- dune/uggrid/numerics/formats.cc
+++ dune/uggrid/numerics/formats.cc
@@ -29,23 +29,12 @@
 /****************************************************************************/
 
 #include <config.h>
-#include <cstdio>
-#include <cstring>
-#include <ctype.h>
 
-#include <dune/uggrid/ugdevices.h>
-#include <dune/uggrid/gm/algebra.h>
-#include <dune/uggrid/gm/enrol.h>
-#include <dune/uggrid/gm/gm.h>
-#include <dune/uggrid/gm/ugm.h>
-#include <dune/uggrid/low/misc.h>
-#include <dune/uggrid/low/scan.h> // for ReadArgvChar
-#include <dune/uggrid/low/ugenv.h>
+#include <dune/uggrid/low/namespace.h>
 #include <dune/uggrid/low/ugstruct.h>
 #include <dune/uggrid/low/ugtypes.h>
 
 #include <dune/uggrid/numerics/formats.h>
-#include <dune/uggrid/numerics/udm.h>
 
 
 USING_UG_NAMESPACES
@@ -72,91 +61,11 @@ USING_UG_NAMESPACES
 /*                                                                          */
 /****************************************************************************/
 
-/** @name Environment dir and var ids */
-/*@{*/
-static INT theNewFormatDirID;                   /* env type for NewFormat dir           */
-/*@}*/
 
-REP_ERR_FILE
-
-
-static INT CleanupTempDir (void)
-{
-  ENVDIR *dir;
-
-  dir = ChangeEnvDir("/newformat");
-  if (dir == NULL) {
-    PrintErrorMessage('E',"CleanupTempDir","/newformat does not exist");
-    REP_ERR_RETURN (1);
-  }
-
-  ChangeEnvDir("/");
-  ENVITEM_LOCKED(dir) = 0;
-  if (RemoveEnvDir((ENVITEM *) dir))
-    REP_ERR_RETURN (1);
-
-  return (0);
-}
-
+// This method is obsolete.  dune-grid still calls it, but that call
+// will be removed eventually.
 INT NS_DIM_PREFIX CreateFormatCmd (INT argc, char **argv)
 {
-  ENVDIR *dir;
-
-  std::string formatname = "DuneFormat" + std::to_string(DIM) + "d";
-
-  if (GetFormat(formatname.c_str()) != nullptr) {
-    PrintErrorMessage('W',"newformat","format already exists");
-    return 0;
-  }
-
-  /* install the /newformat directory */
-  if (ChangeEnvDir("/")==NULL) {
-    PrintErrorMessage('F',"InitFormats","could not changedir to root");
-    REP_ERR_RETURN(__LINE__);
-  }
-  if (MakeEnvItem("newformat",theNewFormatDirID,sizeof(ENVDIR))==NULL)
-  {
-    PrintErrorMessage('F',"InitFormats",
-                      "could not install '/newformat' dir");
-    REP_ERR_RETURN(__LINE__);
-  }
-
-  /* fill degrees of freedom needed */
-  VectorDescriptor vd[MAXVECTORS];
-#ifdef __TWODIM__
-  INT nvd = 0;
-#else
-  INT nvd = 1;
-  vd[0].tp    = SIDEVEC;
-  vd[0].size  = sizeof(DOUBLE);
-  vd[0].name  = 's';
-#endif
-
-  /* create format */
-  FORMAT* newFormat = CreateFormat(nvd,vd);
-  if (newFormat==NULL)
-  {
-    PrintErrorMessage('E',"newformat","failed creating the format");
-    REP_ERR_RETURN (CleanupTempDir());
-  }
-
-  /* move templates into the new directory */
-  dir = ChangeEnvDir("/newformat");
-  if (dir == NULL) {
-    PrintErrorMessage('E',"newformat","failed moving template");
-    REP_ERR_RETURN (4);
-  }
-  if (ENVITEM_DOWN((ENVDIR *)newFormat) != NULL) {
-    PrintErrorMessage('E',"newformat","failed moving template");
-    REP_ERR_RETURN (4);
-  }
-  ENVITEM_DOWN((ENVDIR *)newFormat) = ENVITEM_DOWN(dir);
-  ENVITEM_DOWN(dir) = NULL;
-  ENVITEM_LOCKED(dir) = 0;
-  ChangeEnvDir("/");
-  if (RemoveEnvDir((ENVITEM *)dir))
-    PrintErrorMessage('W',"InitFormats","could not remove newformat dir");
-
   return 0;
 }
 
@@ -176,10 +85,6 @@ INT NS_DIM_PREFIX CreateFormatCmd (INT argc, char **argv)
 
 INT NS_DIM_PREFIX InitFormats ()
 {
-  INT tp;
-
-  theNewFormatDirID = GetNewEnvDirID();
-
   if (MakeStruct(":SparseFormats")!=0) return(__LINE__);
 
   return (0);
