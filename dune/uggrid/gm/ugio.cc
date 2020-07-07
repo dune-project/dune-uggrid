26cbc0ebea2ed0bca8a00a7f12954594c6da93bf
Oliver Sander <oliver.sander@tu-dresden.de>
2020-07-07 21:11:18 +0200

    Remove several unused debugging macros

--- dune/uggrid/gm/ugio.cc
+++ dune/uggrid/gm/ugio.cc
@@ -143,7 +143,6 @@ USING_UGDIM_NAMESPACE
 /*																			*/
 /****************************************************************************/
 
-REP_ERR_FILE
 static INT gridpaths_set=false;
 static MGIO_RR_RULE *rr_rules;
 static INT rr_rule_offsets[TAGS];
