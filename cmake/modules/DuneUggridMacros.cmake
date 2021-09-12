1a7e34358c482b2c9676a19a4a533d7a405c335e
Simon Praetorius <simon.praetorius@tu-dresden.de>
2021-09-12 21:43:59 +0200

    Put CMAKE_MODULE_PATH into <module>-config.cmake and remove warnings

--- cmake/modules/DuneUggridMacros.cmake
+++ cmake/modules/DuneUggridMacros.cmake
@@ -3,19 +3,7 @@ set(UG_FOUND True)
 set(HAVE_UG True)
 set(UG_VERSION "${DUNE_UGGRID_VERSION}")
 
+set(UG_DEFINITIONS)
 if(UG_PARALLEL)
-  # Actually we probably should activate UG
-  # for everything. But for the time being we fall
-  # back to the enable trick. To change this just
-  # uncomment the lines below.
-  #add_definitions("-DENABLE_UG=1")
-  #add_definitions("-DModelP")
-  set(UG_DEFINITIONS "ENABLE_UG=1;ModelP")
-else()
-  # Actually we probably should activate UG
-  # for everything. But for the time being we fall
-  # back to the enable trick. To change this just
-  # uncomment the lines below.
-  #add_definitions("-DENABLE_UG=1")
-  set(UG_DEFINITIONS "ENABLE_UG=1")
+  list(APPEND UG_DEFINITIONS "ModelP")
 endif()
