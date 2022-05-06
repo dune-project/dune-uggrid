dune-project/dune-uggrid

commit 5fffab2217cafd92e1b2b487724010132a32fd18

Remove implementations for NODEVEC, EDGEVEC, ELEMVEC
Dune does not need these Vector types.  They are part
of the UG3 linear algebra, which is not used by Dune.
An exception is the SIDEVEC type (values associated
to sides of 3d elements).  As the UG3 grid data structure
does not explicitly store sides of 3d elements, Dune
abuses SIDEVEC objects to store information about
these sides (level and leaf index, and ids).

dune/uggrid/parallel/dddif/identify.cc
@@ -488,8 +488,8 @@ static void IdentifyNode (GRID *theGrid, ELEMENT *theNeighbor, NODE *theNode,
   /* only new created nodes are identified */
   if (!NEW_NIDENT(theNode)) return;
 
-  if (Vec)
-    if (GetVectorSize(theGrid,NODEVEC,(GEOM_OBJECT *)theNode) == 0)
+//   if (Vec)
+//     if (GetVectorSize(theGrid,NODEVEC,(GEOM_OBJECT *)theNode) == 0)
       Vec = 0;
 
   switch (NTYPE(theNode))
@@ -689,12 +689,6 @@ static INT IdentifySideEdge (GRID *theGrid, EDGE *theEdge, ELEMENT *theElement, 
         #ifdef __THREEDIM__
   IdentObjectHdr[nobject++] = PARHDR(theEdge);
         #endif
-  if (Vec)
-  {
-    if (GetVectorSize(theGrid,EDGEVEC,(GEOM_OBJECT *)theEdge) > 0)
-      if (EDVECTOR(theEdge) != NULL)
-        IdentObjectHdr[nobject++] = PARHDR(EDVECTOR(theEdge));
-  }
 
   /* identify to proclist of neighbor */
   proclist = DDD_InfoProcList(context, PARHDRE(theNeighbor));
@@ -881,10 +875,6 @@ static INT IdentifyEdge (GRID *theGrid,
         #ifdef __THREEDIM__
   IdentObjectHdr[nobject++] = PARHDR(theEdge);
         #endif
-  if (Vec)
-    if (GetVectorSize(theGrid,EDGEVEC,(GEOM_OBJECT *)theEdge) > 0)
-      if (EDVECTOR(theEdge) != NULL)
-        IdentObjectHdr[nobject++] = PARHDR(EDVECTOR(theEdge));
 
         #ifdef __TWODIM__
   /* identify to proclist of neighbor */
@@ -990,15 +980,15 @@ static INT IdentifyObjectsOfElementSide(GRID *theGrid, ELEMENT *theElement,
     theNode = SideNodes[j];
     if (theNode == NULL) continue;
 
-    /* identify new node including its vector and vertex        */
-    IdentifyNode(theGrid,theNeighbor, theNode, SideNodes, j, ncorners,
-                 VEC_DEF_IN_OBJ_OF_GRID(theGrid,NODEVEC));
+//     /* identify new node including its vector and vertex        */
+//     IdentifyNode(theGrid,theNeighbor, theNode, SideNodes, j, ncorners,
+//                  VEC_DEF_IN_OBJ_OF_GRID(theGrid,NODEVEC));
     n++;
   }
   ASSERT(n == nodes);
 
   /* identify edge vectors (2D); edges, edge and side vectors (3D) */
-  if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC) || DIM==3)
+  if (/*VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC) ||*/ DIM==3)
   {
     ELEMENT *SonList[MAX_SONS];
     INT SonsOfSide,SonSides[MAX_SONS];
@@ -1013,11 +1003,12 @@ static INT IdentifyObjectsOfElementSide(GRID *theGrid, ELEMENT *theElement,
 
     for (j=0; j<SonsOfSide; j++) {
 
-      if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC) || DIM==3)
+      if (/*VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC) ||*/ DIM==3)
       {
         INT edgeofside;
         INT nedges = EDGES_OF_SIDE(SonList[j],SonSides[j]);
 
+#if 0
         /* identify the edge and vector */
         for (edgeofside=0; edgeofside<nedges; edgeofside++) {
           EDGE *theEdge;
@@ -1030,6 +1021,7 @@ static INT IdentifyObjectsOfElementSide(GRID *theGrid, ELEMENT *theElement,
           IdentifySideEdge(theGrid, theEdge, theElement, theNeighbor,
                            VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC));
         }
+#endif
       }
 
                         #ifdef __THREEDIM__

dune/uggrid/gm/algebra.h
@@ -133,7 +133,6 @@ INT GetDomainPart (const INT s2p[], const GEOM_OBJECT *obj, INT side);
 
 /** @name basic create and dispose functions */
 /*@{*/
-INT         CreateVector                                    (GRID *theGrid, INT ObjectType, GEOM_OBJECT *object, VECTOR **vHandle);
 INT         CreateSideVector                (GRID *theGrid, INT side, GEOM_OBJECT *object, VECTOR **vHandle);
 INT         ReinspectSonSideVector  (GRID *g, ELEMENT *elem, INT side, VECTOR **vHandle);
 CONNECTION *CreateConnection        (GRID *theGrid, VECTOR *from, VECTOR *to);
@@ -160,10 +159,7 @@ INT             DisposeElementFromElementList (GRID *theGrid, NODE *theNode, ELE
 
 /** @name Query functions */
 /*@{*/
-INT             GetVectorsOfElement                     (const ELEMENT *theElement, INT *cnt, VECTOR **vList);
 INT             GetVectorsOfSides                               (const ELEMENT *theElement, INT *cnt, VECTOR **vList);
-INT             GetVectorsOfEdges                               (const ELEMENT *theElement, INT *cnt, VECTOR **vList);
-INT             GetVectorsOfNodes                               (const ELEMENT *theElement, INT *cnt, VECTOR **vList);
 INT                     GetVectorsOfOType                               (const ELEMENT *theElement, INT type, INT *cnt, VECTOR **vList);
 INT                     DataTypeFilterVList                             (INT dt, VECTOR **vec, INT *cnt);
 INT                     GetVectorsOfDataTypesInObjects  (const ELEMENT *theElement, INT dt, INT obj, INT *cnt, VECTOR *VecList[]);

dune/uggrid/gm/elements.cc
@@ -832,11 +832,6 @@ static INT ProcessElementDescription (MULTIGRID *theMG, GENERAL_ELEMENT *el)
 
   /* element vector */
   evector_offset[tag] = 0;
-  if (VEC_DEF_IN_OBJ_OF_MG(theMG,ELEMVEC))
-  {
-    evector_offset[tag] = p_count;
-    p_count++;
-  }
 
   /* side vector */
   svector_offset[tag] = 0;

dune/uggrid/gm/enrol.cc
@@ -123,7 +123,7 @@ std::unique_ptr<FORMAT> NS_DIM_PREFIX CreateFormat ()
       po2t[i][j] = NOVTYPE;
 
 #ifdef __THREEDIM__
-  po2t[0][3] = SIDEVEC;
+  po2t[0][0] = SIDEVEC;
 #endif
 
   SHORT MatStorageNeeded[MAXCONNECTIONS];
@@ -230,10 +230,7 @@ std::unique_ptr<FORMAT> NS_DIM_PREFIX CreateFormat ()
     /* set connection depth information */
     FMT_CONN_DEPTH_TP(fmt,type) = mDesc[i].depth;
     MaxDepth = MAX(MaxDepth,mDesc[i].depth);
-    if ((FMT_TYPE_USES_OBJ(fmt,mDesc[i].from,ELEMVEC))&&(FMT_TYPE_USES_OBJ(fmt,mDesc[i].to,ELEMVEC)))
-      NeighborhoodDepth = MAX(NeighborhoodDepth,mDesc[i].depth);
-    else
-      NeighborhoodDepth = MAX(NeighborhoodDepth,mDesc[i].depth+1);
+    NeighborhoodDepth = MAX(NeighborhoodDepth,mDesc[i].depth+1);
 
   }
   FMT_CONN_DEPTH_MAX(fmt) = MaxDepth;

dune/uggrid/gm/gm.h
@@ -179,9 +179,9 @@ enum {MAX_SONS = 30};
 /****************************************************************************/
 
 /** \brief Number of different data types                                    */
-#define MAXVOBJECTS                                             4
+#define MAXVOBJECTS                                             1
 /** \brief max number of abstract vector types                  */
-#define MAXVECTORS                                              4
+#define MAXVECTORS                                              1
 #if (MAXVECTORS<MAXVOBJECTS)
         #error *** MAXVECTORS must not be smaller than MAXVOBJECTS ***
 #endif
@@ -205,9 +205,6 @@ enum {MAX_SONS = 30};
 
 /** \brief Type of geometric entity which a certain vector is attached to */
 enum VectorType {NOVTYPE=-1,  //** Undefined */
-                 NODEVEC,   /**< Vector associated to a node */
-                 EDGEVEC,   /**< Vector associated to an edge */
-                 ELEMVEC,   /**< Vector associated to an element */
                  SIDEVEC    /**< Vector associated to an element side */
 };
 

dune/uggrid/gm/ugio.cc
@@ -1139,7 +1139,6 @@ static INT WriteElementParInfo (GRID *theGrid,
   int *pl;
   NODE *theNode;
   VERTEX *theVertex;
-  EDGE *theEdge;
 #ifdef ModelP
   auto& dddContext = theGrid->dddContext();
 #endif
@@ -1200,35 +1199,10 @@ static INT WriteElementParInfo (GRID *theGrid,
     pinfo->v_ident[k] = VXGID(theVertex);
   }
 
-#ifdef __TWODIM__
-  if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC))
-  {
-    VECTOR *v;
-    for (k=0; k<EDGES_OF_ELEM(theElement); k++) {
-      theEdge=GetEdge(CORNER(theElement,CORNER_OF_EDGE(theElement,k,0)),
-                      CORNER(theElement,CORNER_OF_EDGE(theElement,k,1)));
-      v = EDVECTOR(theEdge);
-      pinfo->prio_edge[k] = PRIO(v);
-      pinfo->ncopies_edge[k] = NCOPIES(dddContext, v);
-      if (n_max<pinfo->ncopies_edge[k]+s)
-      {
-        PrintErrorMessage('E',"WriteElementParInfo","increase PROCLISTSIZE in gm/ugio.c\n");
-        REP_ERR_RETURN(1);
-      }
-      pinfo->ed_ident[k] = GID(v);
-      if (pinfo->ncopies_edge[k]>0) {
-        pl = PROCLIST(dddContext, v);
-        for (i=0,j=2; i<pinfo->ncopies_edge[k]; i++,j+=2)
-          ActProcListPos[s++] = pl[j];
-      }
-    }
-  }
-#endif
-
 #ifdef __THREEDIM__
   for (k=0; k<EDGES_OF_ELEM(theElement); k++)
   {
-    theEdge = GetEdge(CORNER(theElement,CORNER_OF_EDGE(theElement,k,0)),
+    EDGE* theEdge = GetEdge(CORNER(theElement,CORNER_OF_EDGE(theElement,k,0)),
                       CORNER(theElement,CORNER_OF_EDGE(theElement,k,1)));
     pinfo->prio_edge[k] = PRIO(theEdge);
     pinfo->ncopies_edge[k] = NCOPIES(dddContext, theEdge);
@@ -1719,25 +1693,19 @@ INT NS_DIM_PREFIX SaveMultiGrid (MULTIGRID *theMG, const char *name, const char 
 static INT Evaluate_pinfo (GRID *theGrid, ELEMENT *theElement, MGIO_PARINFO *pinfo)
 {
   INT i,j,s,prio,where,oldwhere;
-  INT evec,nvec,edvec,svec;
   GRID            *vgrid;
   ELEMENT         *theFather,*After,*Next,*Succe;
   NODE            *theNode;
   VERTEX          *theVertex;
-  VECTOR          *theVector;
-  EDGE            *theEdge;
 
 #ifdef ModelP
   auto& dddContext = theGrid->dddContext();
 #endif
 
-  evec = VEC_DEF_IN_OBJ_OF_MG(MYMG(theGrid),ELEMVEC);
-  nvec = VEC_DEF_IN_OBJ_OF_MG(MYMG(theGrid),NODEVEC);
-  edvec = VEC_DEF_IN_OBJ_OF_MG(MYMG(theGrid),EDGEVEC);
 #ifdef __THREEDIM__
-  svec = VEC_DEF_IN_OBJ_OF_MG(MYMG(theGrid),SIDEVEC);
+  INT svec = VEC_DEF_IN_OBJ_OF_MG(MYMG(theGrid),SIDEVEC);
 #else
-  svec = 0;
+  INT svec = 0;
 #endif
   /* this function does not support side vectors                        */
   /* proclist and identificator need to be stored for each  side vector */
@@ -1768,19 +1736,10 @@ static INT Evaluate_pinfo (GRID *theGrid, ELEMENT *theElement, MGIO_PARINFO *pin
     }
     else
       GRID_LINK_ELEMENT(theGrid,theElement,prio);
-    if (evec)
-    {
-      theVector = EVECTOR(theElement);
-      GRID_UNLINK_VECTOR(theGrid,theVector);
-      SETPRIO(dddContext, EVECTOR(theElement),prio);
-      GRID_LINK_VECTOR(theGrid,theVector,prio);
-    }
   }
   for (i=0; i<pinfo->ncopies_elem; i++)
   {
     DDD_IdentifyNumber(dddContext, PARHDRE(theElement),pinfo->proclist[s],pinfo->e_ident);
-    if (evec)
-      DDD_IdentifyNumber(dddContext, PARHDR(EVECTOR(theElement)),pinfo->proclist[s],pinfo->e_ident);
     s++;
   }
 
@@ -1794,20 +1753,11 @@ static INT Evaluate_pinfo (GRID *theGrid, ELEMENT *theElement, MGIO_PARINFO *pin
         GRID_UNLINK_NODE(theGrid,theNode);
         SETPRIO(dddContext, theNode,prio);
         GRID_LINK_NODE(theGrid,theNode,prio);
-        if (nvec)
-        {
-          theVector = NVECTOR(theNode);
-          GRID_UNLINK_VECTOR(theGrid,theVector);
-          SETPRIO(dddContext, NVECTOR(theNode),prio);
-          GRID_LINK_VECTOR(theGrid,theVector,prio);
-        }
       }
       PRINTDEBUG(gm,1,("Evaluate-pinfo():nid=%d prio=%d\n",ID(theNode),prio);fflush(stdout));
       for (i=0; i<pinfo->ncopies_node[j]; i++)
       {
         DDD_IdentifyNumber(dddContext, PARHDR(theNode),pinfo->proclist[s],pinfo->n_ident[j]);
-        if (nvec)
-          DDD_IdentifyNumber(dddContext, PARHDR(NVECTOR(theNode)),pinfo->proclist[s],pinfo->n_ident[j]);
         s++;
       }
       SETUSED(theNode,1);
@@ -1839,54 +1789,19 @@ static INT Evaluate_pinfo (GRID *theGrid, ELEMENT *theElement, MGIO_PARINFO *pin
       s += pinfo->ncopies_vertex[j];
   }
 
-#ifdef __TWODIM__
-  if (edvec) {
-    for (j=0; j<EDGES_OF_ELEM(theElement); j++) {
-      theEdge=GetEdge(CORNER(theElement,CORNER_OF_EDGE(theElement,j,0)),
-                      CORNER(theElement,CORNER_OF_EDGE(theElement,j,1)));
-      if (USED(theEdge) == 0) {
-        theVector = EDVECTOR(theEdge);
-        if ((prio = pinfo->prio_edge[j]) != PrioMaster) {
-          GRID_UNLINK_VECTOR(theGrid,theVector);
-          SETPRIO(dddContext, theVector,prio);
-          GRID_LINK_VECTOR(theGrid,theVector,prio);
-        }
-        for (i=0; i<pinfo->ncopies_edge[j]; i++) {
-          DDD_IdentifyNumber(dddContext, PARHDR(theVector),
-                             pinfo->proclist[s],pinfo->ed_ident[j]);
-          s++;
-        }
-        SETUSED(theEdge,1);
-      }
-      else
-        s += pinfo->ncopies_edge[j];
-    }
-  }
-#endif
-
 #if (MGIO_DIM==3)
   for (j=0; j<EDGES_OF_ELEM(theElement); j++)
   {
-    theEdge = GetEdge(CORNER_OF_EDGE_PTR(theElement,j,0),
+    EDGE* theEdge = GetEdge(CORNER_OF_EDGE_PTR(theElement,j,0),
                       CORNER_OF_EDGE_PTR(theElement,j,1));
     if (USED(theEdge) == 0)
     {
       if ((prio = pinfo->prio_edge[j]) != PrioMaster)
-      {
         SETPRIO(dddContext, theEdge,prio);
-        if (edvec)
-        {
-          theVector = EDVECTOR(theEdge);
-          GRID_UNLINK_VECTOR(theGrid,theVector);
-          SETPRIO(dddContext, EDVECTOR(theEdge),prio);
-          GRID_LINK_VECTOR(theGrid,theVector,prio);
-        }
-      }
+
       for (i=0; i<pinfo->ncopies_edge[j]; i++)
       {
         DDD_IdentifyNumber(dddContext, PARHDR(theEdge),pinfo->proclist[s],pinfo->ed_ident[j]);
-        if (edvec)
-          DDD_IdentifyNumber(dddContext, PARHDR(EDVECTOR(theEdge)),pinfo->proclist[s],pinfo->ed_ident[j]);
         s++;
       }
       SETUSED(theEdge,1);

dune/uggrid/gm/ugm.cc
@@ -411,11 +411,9 @@ static NODE *CreateNode (GRID *theGrid, VERTEX *vertex,
                          GEOM_OBJECT *Father, INT NodeType, INT with_vector)
 {
   NODE *pn;
-  VECTOR *pv;
   INT size;
 
-  size = sizeof(NODE);
-  if (!VEC_DEF_IN_OBJ_OF_GRID(theGrid,NODEVEC)) size -= sizeof(VECTOR *);
+  size = sizeof(NODE) - sizeof(VECTOR *);
 
   pn = (NODE *)GetMemoryForObject(MYMG(theGrid),size,NDOBJ);
   if (pn==NULL) return(NULL);
@@ -457,21 +455,6 @@ static NODE *CreateNode (GRID *theGrid, VERTEX *vertex,
   }
   else
     SETNSUBDOM(pn,0);
-
-  if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,NODEVEC))
-  {
-    if (with_vector)
-    {
-      if (CreateVector (theGrid,NODEVEC,(GEOM_OBJECT *)pn,&pv))
-      {
-        DisposeNode (theGrid,pn);
-        return (NULL);
-      }
-      NVECTOR(pn) = pv;
-    }
-    else
-      NVECTOR(pn) = NULL;
-  }
 
   theGrid->status |= 1;          /* recalculate stiffness matrix */
 
@@ -2074,7 +2057,6 @@ CreateEdge (GRID *theGrid, ELEMENT *theElement, INT edge, bool with_vector)
   EDGE *pe,*father_edge;
   NODE *from,*to,*n1,*n2;
   LINK *link0,*link1;
-  VECTOR *pv;
 #ifdef __THREEDIM__
   VERTEX *theVertex;
   NODE *nbn1,*nbn2,*nbn3,*nbn4;
@@ -2094,10 +2076,7 @@ CreateEdge (GRID *theGrid, ELEMENT *theElement, INT edge, bool with_vector)
     return(pe);
   }
 
-  if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC))
-    pe = (EDGE*)GetMemoryForObject(theGrid->mg,sizeof(EDGE),EDOBJ);
-  else
-    pe = (EDGE*)GetMemoryForObject(theGrid->mg,sizeof(EDGE)-sizeof(VECTOR*),EDOBJ);
+  pe = (EDGE*)GetMemoryForObject(theGrid->mg,sizeof(EDGE)-sizeof(VECTOR*),EDOBJ);
   if (pe==NULL) return(NULL);
 
   /* initialize data */
@@ -2298,20 +2277,6 @@ CreateEdge (GRID *theGrid, ELEMENT *theElement, INT edge, bool with_vector)
     }     /* end switch */
   }   /* end (theFather!=NULL) */
 
-  /* create vector if */
-  if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC))
-  {
-    if (with_vector) {
-      if (CreateVector (theGrid,EDGEVEC,(GEOM_OBJECT *)pe,&pv)) {
-        DisposeEdge (theGrid,pe);
-        return (NULL);
-      }
-      EDVECTOR(pe) = pv;
-    }
-    else
-      EDVECTOR(pe) = NULL;
-  }
-
   /* put in neighbor lists */
   NEXT(link0) = START(from);
   START(from) = link0;
@@ -2438,22 +2403,6 @@ ELEMENT * NS_DIM_PREFIX CreateElement (GRID *theGrid, INT tag, INT objtype, NODE
            }
            UserWriteF("\n");)
 
-
-  /* create element vector if */
-  if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,ELEMVEC))
-  {
-    if (with_vector)
-    {
-      if (CreateVector (theGrid,ELEMVEC,(GEOM_OBJECT *)pe,&pv))
-      {
-        DisposeElement(theGrid,pe,true);
-        return (NULL);
-      }
-      SET_EVECTOR(pe,pv);
-    }
-    else
-      SET_EVECTOR(pe,NULL);
-  }
 
   /* create side vectors if */
   if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,SIDEVEC))
@@ -3083,15 +3032,7 @@ static INT DisposeEdge (GRID *theGrid, EDGE *theEdge)
   if (MIDNODE(theEdge) != NULL)
     SETNFATHER(MIDNODE(theEdge),NULL);
 
-  /* dispose vector and its matrices from edge-vector if */
-  if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC))
-  {
-    if (DisposeVector (theGrid,EDVECTOR(theEdge)))
-      RETURN(1);
-    PutFreeObject(theGrid->mg,theEdge,sizeof(EDGE),EDOBJ);
-  }
-  else
-    PutFreeObject(theGrid->mg,theEdge,sizeof(EDGE)-sizeof(VECTOR*),EDOBJ);
+  PutFreeObject(theGrid->mg,theEdge,sizeof(EDGE)-sizeof(VECTOR*),EDOBJ);
 
   /* check error condition */
   if (found!=2) RETURN(1);
@@ -3202,13 +3143,7 @@ INT NS_DIM_PREFIX DisposeNode (GRID *theGrid, NODE *theNode)
 
   /* dispose vector and its matrices from node-vector */
   size = sizeof(NODE);
-  if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,NODEVEC))
-  {
-    if (DisposeVector (theGrid,NVECTOR(theNode)))
-      RETURN(1);
-  }
-  else
-    size -= sizeof(VECTOR *);
+  size -= sizeof(VECTOR *);
   PutFreeObject(theGrid->mg,theNode,size,NDOBJ);
 
   /* return ok */
@@ -3555,11 +3490,6 @@ INT NS_DIM_PREFIX DisposeElement (GRID *theGrid, ELEMENT *theElement, INT dispos
             #endif
     }
   }
-
-  /* dispose vector in center of element */
-  if (VEC_DEF_IN_OBJ_OF_GRID(theGrid,ELEMVEC))
-    if (DisposeVector (theGrid,EVECTOR(theElement)))
-      RETURN(1);
 
 #ifdef ModelP
   /* free message buffer */
@@ -6164,28 +6094,6 @@ void NS_DIM_PREFIX ListVector (const MULTIGRID *theMG, const VECTOR *theVector, 
   if (READ_FLAG(modifiers,LV_VO_INFO))
     switch (VOTYPE(theVector))
     {
-    case NODEVEC :
-    {
-      theNode = (NODE*)VOBJECT(theVector);
-                                #if defined __OVERLAP2__
-      if ( theNode == NULL )
-        UserWriteF("NODE-V NULL                ");
-      else
-                                #endif
-      UserWriteF("NODE-V nodeID=" ID_FMTX
-                 "                ",
-                 ID_PRTX(theNode));
-    }
-    break;
-    case EDGEVEC :
-    {
-      theEdge = (EDGE*)VOBJECT(theVector);
-      UserWriteF("EDGE-V fromID=" ID_FFMT
-                 " to__ID=%7ld ",
-                 ID_PRT(NBNODE(LINK0(theEdge))),
-                 ID(NBNODE(LINK1(theEdge))));
-    }
-    break;
                 #ifdef __THREEDIM__
     case SIDEVEC :
     {
@@ -6196,14 +6104,6 @@ void NS_DIM_PREFIX ListVector (const MULTIGRID *theMG, const VECTOR *theVector, 
     }
     break;
                 #endif
-    case ELEMVEC :
-    {
-      theElement = (ELEMENT*)VOBJECT(theVector);
-      UserWriteF("ELEM-V elemID=" EID_FFMT
-                 "                ",
-                 EID_PRT(theElement));
-    }
-    break;
 
     default : PrintErrorMessage( 'E', "ListVector", "unrecognized VECTOR type" );
       assert(0);

commit abc74711172c1ef7575b077b5c362987c6096c09

identify nodes and edges again
Regression introduced in 5fffab2217cafd92e1b2b487724010132a32fd18

dune/uggrid/parallel/dddif/identify.cc
@@ -983,6 +983,7 @@ static INT IdentifyObjectsOfElementSide(GRID *theGrid, ELEMENT *theElement,
 //     /* identify new node including its vector and vertex        */
 //     IdentifyNode(theGrid,theNeighbor, theNode, SideNodes, j, ncorners,
 //                  VEC_DEF_IN_OBJ_OF_GRID(theGrid,NODEVEC));
+     IdentifyNode(theGrid,theNeighbor, theNode, SideNodes, j, ncorners, 0);
     n++;
   }
   ASSERT(n == nodes);
@@ -1008,7 +1009,6 @@ static INT IdentifyObjectsOfElementSide(GRID *theGrid, ELEMENT *theElement,
         INT edgeofside;
         INT nedges = EDGES_OF_SIDE(SonList[j],SonSides[j]);
 
-#if 0
         /* identify the edge and vector */
         for (edgeofside=0; edgeofside<nedges; edgeofside++) {
           EDGE *theEdge;
@@ -1018,10 +1018,10 @@ static INT IdentifyObjectsOfElementSide(GRID *theGrid, ELEMENT *theElement,
                             CORNER_OF_EDGE_PTR(SonList[j],edge,1));
           ASSERT(theEdge!=NULL);
 
-          IdentifySideEdge(theGrid, theEdge, theElement, theNeighbor,
-                           VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC));
+//          IdentifySideEdge(theGrid, theEdge, theElement, theNeighbor,
+//                           VEC_DEF_IN_OBJ_OF_GRID(theGrid,EDGEVEC));
+          IdentifySideEdge(theGrid, theEdge, theElement, theNeighbor, 0);
         }
-#endif
       }
 
                         #ifdef __THREEDIM__