181821685e772e678e7db47c962e736ab90d6e46
Oliver Sander <oliver.sander@tu-dresden.de>
2023-11-22 12:02:09 +0100

    Remove methods that print control words or control entries
    These are never used.

--- dune/uggrid/gm/gm.h
+++ dune/uggrid/gm/gm.h
@@ -3173,9 +3173,6 @@ INT             CheckSubdomains                 (MULTIGRID *theMG);
 /* multigrid user data space management (using the heaps.c block heap management) */
 INT             AllocateControlEntry    (INT cw_id, INT length, INT *ce_id);
 INT             FreeControlEntry                (INT ce_id);
-void            ListCWofObject                  (const void *obj, UINT offset);
-void            ListAllCWsOfObject              (const void *obj);
-void            ListAllCWsOfAllObjectTypes (PrintfProcPtr myprintf);
 UINT ReadCW                                     (const void *obj, INT ce);
 void            WriteCW                                 (void *obj, INT ce, INT n);
 
